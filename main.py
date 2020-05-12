eb3ff88a76f9c8485f08bcef63fdf9fbad8acd00
ANameWithNoName <50911451+ANameWithNoName@users.noreply.github.com>
2020-05-12 22:23:12 +0300

    Am mai scris cate ceva, cu bani cu d astea. NU am testat foarte probabil sa fie bug uit ca dracu. Mai trebuie implementate mai frumos functiile si sa incepem sa mutam in alte module ca nu se mai intelege ce naiba e pe acolo

--- main.py
+++ main.py
@@ -17,24 +17,31 @@ def DistancesofCities():
   distances.sort()
   return distances
 
+#determines the price for food and rent in every town
 def PricesInTown(nbcities):
+  prices = random.sample(range(5,50),nbcities)
+  return prices
+
+def RentInTown(nbcities):
   prices = random.sample(range(5,50),nbcities)
   return prices
 
 class Character:
   #atributes of our braves heroes
-  def __init__(self, food, life, daystrav, stamina, money):
+  def __init__(self, food, life, daystrav, stamina, money, dayspassed):
     self.food = food
     self.life = life
     self.daystrav = daystrav
     self.money = money
     self.stamina = stamina
+    self.dayspassed = dayspassed
 
   #another day passes
   def DayPass(self, foodperday):
     self.food -= foodperday #loses some food as a day passes
     self.stamina -= 1 #traveling is tiring even in 2848
     self.daystrav += 1
+    self.dayspassed += 1
     return self
 
   #checks if players is still alive
@@ -43,14 +50,15 @@ class Character:
 
   #resting is good for your welbeing
   #increases you stamina, some time passes, you eat some food
-  def RestInTown(self, foodperday):
-    self.daystrav += 1 #another day passes while resting
+  def RestInTown(self, foodperday, checkpoint, prices):
+    self.dayspassed += 1 #another day passes while resting
     self.stamina  += 3 #stamina regained resting
     self.food -= foodperday
     return self
 
+  #ARE NEVOIE DE ASIGURARE (PROB CEVA TRY EXCEPT) IN CAZUL IN CARE NU ARE DESTUI BANI FRAIERUL
   def RestInField(self, foodperday):
-    self.daystrav += 1 #another day passes while resting
+    self.dayspassed += 1 #another day passes while resting
     self.stamina  += 2 #stamina regained resting
     self.food -= foodperday
     return self
@@ -58,16 +66,30 @@ class Character:
  #function for when the player reaches the town
  # it prints some messages, lets him know what he can in this new town
  # gets some info (e.g. the prices in the town)
-def ActionsInTown(checkpoint, prices):
+def ActionsInTown(player, checkpoint, prices):
   print("You have reached a new town!")
-  print("Press S if you want to rest here for some time, but beware! You only have food left for "\
-    ,player.food // foodperday,"days")
-  print("The merchants in this town will sell you some food for ", ,"enemy skalps")
+  print("You only have food left for "\
+    ,player.food // foodperday,"days.\n You have energy to travel for another ",player.stamina" days))
+  print("The merchants in this town will sell you some food for ", prices[checkpoint] ,"enemy skalps per unit of food\n")
+  print("The following actions are available in this town:\n" \
+    "1. Rest (press S) - you regain some stamina. The rent is ",rent[checkpoint,\
+      "2. Buy some food - press B"])
+    return 0
+
+#function to buy food, gets the idx of the city, the price list, the player class
+#and the quantity of food the players wants to buy
+#returns the player class with modified values for food, money
+def BuyingFood(player, checkpoint, prices, amount):
+  player.money -= prices[checkpoint] * amount
+  player.food += amount 
+  return player
 
 player = Character(100, 1,0, 10) # food, life, daystrav, stamina
 townloc = DistancesofCities() #generates the list with town locations from starting point
 checkpoint = 0 #the index of the next city visited to be visited
 foodperday = 10
+prices = PricesInTown()
+rent = RentInTown()
 
 print("You have found an old map! The cities are located at these distances from your location: ")
 print("Beware! You are not made of iron. You lose 1 stamina for everyday spent travelling, and you gain 2 stamina back \
@@ -75,25 +97,45 @@ print("Beware! You are not made of iron. You lose 1 stamina for everyday spent t
 print(townloc)
 
 while True:
-  if player.DeadPlayer() != 0:
-    print("Day ", player.daystrav ,". You have ",player.food," food left and ", player.life, "lifepoints. You have already travelled ",player.daystrav\
+
+  if player.DeadPlayer() != 0: #checks if game can continue
+
+    print("Day ", player.dayspassed". You have travelled so far ",player.daystrav. " days.\n" ,". You have ",player.food\
+      ," food left and ", player.life, "lifepoints. You have already travelled ",player.daystrav\
       ,"days. \n You have stamina to travel for another: ",player.stamina,"days."\
         " You'll reach the next town in: ",townloc[checkpoint] - player.daystrav," days")
+
     userInput = input("Press enter to reach a new day or if you feel tired you can get some rest by pressing s: ") 
-    if userInput == 's' or 'S':
+
+    if userInput == 's' or 'S': #resting
       player = player.RestInField(foodperday)
-    if userInput == '':
+
+    if userInput == '': #another day passes
       player = player.DayPass(foodperday)
-      if townloc[checkpoint] == player.daystrav:
-        print("You have reached a new town!")
-        print("Press S if you want to rest here for some time, but beware! You only have food left for "\
-          ,player.food // foodperday,"days")
-        print("The merchants in this town will sell you some food for ", ,"enemy skalps")
+      if townloc[checkpoint] == player.daystrav: # when the player reaches a new town (travels the distance to the new town)
+        ActionsInTown(player, checkpoint, prices)
         userInput = input()
+
         if userInput == 's' or 'S':
-          player = player.RestInTown(foodperday)
-        checkpoint += 1 #another town waits for you at the horizon
-      if checkpoint == len(townloc):
+          ok = False
+          while ok == False:
+            if rent[checkpoint] <= player.money:
+              player = player.RestInTown(foodperday, checkpoint, prices)
+              ok = True # un try except cred CA AR MERGE, dar sunt obosit acum :/
+
+        if userInput == 'b' or 'B':
+          ok = False
+          while ok == False:
+            amount = input("How much food do you want to buy? ")
+            if amount * price[checkpoint] <= player.money: # CUM SE POATE ADAUGA VERIFICAREA ASTA LA FUNCTIE OARE?
+              player = player.BuyingFood(foodperday, player)
+              ok = True #break
+            
+
+          
+        checkpoint += 1 #we are done with this town, we are going to the next one
+
+      if checkpoint == len(townloc): # the player reaches the destination town
         print("Congratulations! You have reached your destination!")
         break
   else:
