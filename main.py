37e543300a0e98303abe6d29cf529ce276be620d
thecrazyapple01 <65122166+thecrazyapple01@users.noreply.github.com>
2020-05-12 20:45:38 +0200

    Test of car moving left

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 import pygame as pg
 
+height_px = 500
+width_px = 600
+reso = (width_px, height_px)
+black = (0, 0, 0)
+grey = (127, 127, 127)
+
 pg.init()
-
-# some code here
-
-height_px = 600
-width_px = 500
-reso = (height_px, width_px)
 
 pg.init()
 
 screen = pg.display.set_mode(reso)
+screen.fill(grey)
+
+car = pg.image.load("images/car.png")
+carimg = pg.transform.scale(car, (100, 100))
+carrect = carimg.get_rect()
+carrect.centerx = 500
+carrect.centery = 250
+screen.blit(carimg, carrect)
+pg.display.flip()
+
+for t in range(12000):
+    if t % 20 == 0:
+        carrect.centerx -= 1
+        screen.fill(grey)
+        screen.blit(carimg, carrect)
+    pg.display.flip()
+
+
 
 pg.quit()
 
