26b312ae190e3135438c514766cefb63ecaab725
ANameWithNoName <50911451+ANameWithNoName@users.noreply.github.com>
2020-05-12 21:10:47 +0300

    12 mai am facut o ciorba

--- main.py
+++ main.py
@@ -3,61 +3,70 @@
 import sys
 import random
 
-#function which returns a random number of cities at random distances
-def DistancesofCities():
-  nbcities = random.randint(3, 5) #between 5 and 15 cities
-  distances = random.sample(range(5,100), nbcities) #min distance 5 km, max distance 100 km
-  distances.sort()
-  return distances
-
-#function which is closes the program
+#function which closes the program
 def ProgramClosing():
     ProgramClosing = input('Press enter to end the program.')
     if ProgramClosing == '':
         print('Closing program...')
         sys.exit()
 
+#function which returns a random number of cities at random distances
+def DistancesofCities():
+  nbcities = random.randint(3, 5) #between 3 and 5 cities
+  distances = random.sample(range(2,15), nbcities) #min distance 5 km, max distance 100 km
+  distances.sort()
+  return distances
+
 class Character:
-  def __init__(self, food, life, kmtrav):
+  def __init__(self, food, life, daystrav, stamina):
     self.food = food
     self.life = life
-    self.kmtrav = kmtrav
+    self.daystrav = daystrav
+    self.stamina = stamina
 
   def DayPass(self, foodperday):
-    self.food -= foodperday #loses 10 food as a day passes
+    self.food -= foodperday #loses some food as a day passes
     return self.food
 
-player = Character(100, 1,0) # the player starts with 100 food and one life (Yolo) in point 0
+  def DeadPlayer(self):
+    return 0 if player.food or player.stamina == 0 else 1
+player = Character(100, 1,0, 10) # the player starts with 100 food and one life (Yolo) in point 0
 townloc = DistancesofCities() #generates the list with town locations from starting point
-speed = 5 #km per day
-days = 1
 checkpoint = 0 #the index of the next city visited to be visited
-
+dailyfood = 10
 
 print("You have found an old map! The cities are located at these distances from your location: ")
+print("Beware! You are not made of iron. You lose 1 stamina for everyday spent travelling, and you gain 2 stamina back \
+  for everyday spent resting in a town along your long way to the Cyborg Dorado.")
 print(townloc)
 
 while True:
-  if player.food != 0:
-    print("Day ", days,". You have ",player.food," food left and ", player.life, "lifepoints. You have already travelled ",player.kmtrav," km")
-    userInput = input("Press enter to reach a new day ")
+  if player.DeadPlayer() != 0:
+    print("Day ", player.daystrav ,". You have ",player.food," food left and ", player.life, "lifepoints. You have already travelled ",player.daystrav\
+      ,"days. \n You have stamina to travel for another: ",player.stamina,"days.")
+    userInput = input("Press enter to reach a new day ") 
     if userInput == '':
-      player.food = player.DayPass(10)
-      player.kmtrav += speed * 1 #only one day passed
-      days += 1
-      if checkpoint < len(townloc):
-        if townloc[checkpoint] <= player.kmtrav:
-          print("You have reached a new town!")
-          player.food += 10 #food collected from the town
-          checkpoint += 1
-        if checkpoint == len(townloc):
-          print("Congratulations! You have reached your destination!")
-          break
+      player.food = player.DayPass(dailyfood) # you need some food to get by
+      player.daystrav += 1 #only one day passed
+      player.stamina -= 1 #travel is tiring, even in 2848
+      if townloc[checkpoint] == player.daystrav:
+        print("You have reached a new town!")
+        print("Press S if you want to rest here for some time, but beware! You only have food left for "\
+          ,player.food // dailyfood,"days")
+        userInput = input()
+        if userInput == 's' or 'S':
+          player.daystrav += 1
+          player.stamina += 2 # you get some stamina resting
+        player.food += 10 #food collected from the town
+        checkpoint += 1 #another town waits for you at the horizon
+      if checkpoint == len(townloc):
+        print("Congratulations! You have reached your destination!")
+        break
   else:
     player.life -= 1
     if player.life == 0:
       print("Your life reached zero. You died. Please desintall this game as you are not worthy to play\
-        it ever again, loser.")
+        it ever again. Return to your boring games, loser.")
       break
 
 ProgramClosing()
