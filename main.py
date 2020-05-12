2d7e3eb5b0ac3f177c217eab0b298afc6d0e6ede
ANameWithNoName <50911451+ANameWithNoName@users.noreply.github.com>
2020-05-12 19:41:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,37 @@
+'''Oregon Trail BackEnd'''
 
-print("Salut, Andrei!")
+import sys
+
+#function which is closes the program
+def ProgramClosing():
+    ProgramClosing = input('Press enter to end the program.')
+    if ProgramClosing == '':
+        print('Closing program...')
+        sys.exit()
+
+class Character:
+  def __init__(self, food, life):
+    self.food = food
+    self.life = life
+
+  def DayPass(self):
+    self.food -= 10 #loses 10 food as a day passes
+    return self.food
+
+player = Character(100, 1)
+
+t = 1
+
+while True:
+  if player.food != 0:
+    print("Day ", t,". You have ",player.food," food left.")
+    userInput = input("Press enter to reach a new day")
+    if userInput == '':
+      player.food = player.DayPass()
+      t += 1
+  else:
+    print("You died.")
+    break
+
+ProgramClosing()
+
