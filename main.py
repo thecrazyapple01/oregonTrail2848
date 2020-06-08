1f31a93cc5d1fd4e02c9a2bef668e9daff2893f6
thecrazyapple01 <65122166+thecrazyapple01@users.noreply.github.com>
2020-06-08 23:34:19 +0200

    Transformed functions to classed and made the "instructions" menu workable

--- main.py
+++ main.py
@@ -11,69 +11,57 @@ reso = (width_px, height_px)
 black = (0, 0, 0)
 grey = (127, 127, 127)
 white = (255, 255, 255)
-font = pg.font.SysFont('Arial', 30)
+fontArial30 = pg.font.SysFont('Arial', 30)
+texts = []
 
 
 def draw_background():
     screen.fill(black)
 
 
-def draw_menu():
-    # Start new game option
-    text_start = font.render("Start a new game [SPACE]", True, white)
-    text_start_rect = text_start.get_rect()
-    text_start_rect.centerx = int(width_px / 2)
-    text_start_rect.centery = int(height_px / 8)
-    screen.blit(text_start, text_start_rect)
+class TextToDraw():
+    def __init__(self, name="", text="", posx=int(width_px / 2), posy=int(height_px / 2), color=white, font=fontArial30):
+        self.name = str(name)
+        self.text = str(text)
+        self.centerx = int(posx)
+        self.centery = int(posy)
+        self.color = color
+        self.font = font
+        # self.visible = False -- not sure if I need it
 
-    # Instructions option
-    text_instr = font.render("Read the instructions [I]", True, white)
-    text_instr_rect = text_instr.get_rect()
-    text_instr_rect.centerx = int(width_px / 2)
-    text_instr_rect.centery = int(height_px * 3 / 8)
-    screen.blit(text_instr, text_instr_rect)
-
-
-def draw_exit_menu():
-    # Main question
-    text_main = font.render("Do you really want to exit? [Y/N]", True, white)
-    text_main_rect = text_main.get_rect()
-    text_main_rect.centerx = int(width_px / 2)
-    text_main_rect.centery = int(height_px / 2)
-    screen.blit(text_main, text_main_rect)
+    def draw(self):
+        text_surface = self.font.render(self.text, True, self.color)
+        text_rect = text_surface.get_rect()
+        text_rect.centerx = self.centerx
+        text_rect.centery = self.centery
+        screen.blit(text_surface, text_rect)
 
 
-def draw_instructions():
-    #Main text
-    text_main = font.render("Here instructions will be written...", True, white)
-    text_main_rect = text_main.get_rect()
-    text_main_rect.centerx = int(width_px / 2)
-    text_main_rect.centery = int(height_px / 4)
-    screen.blit(text_main, text_main_rect)
-
+# Initialise texts
+texts.append(TextToDraw("main_start", "Start a new game [SPACE]", posy=int(height_px / 8)))
+texts.append(TextToDraw("main_instructions", "Read the instructions [I]", posy=int(height_px * 3 / 8)))
+texts.append(TextToDraw("exit_main", "Do you really want to exit? [Y/N]"))
+texts.append(TextToDraw("instructions_line1", "Here instructions will be written...", posy=int(height_px / 4)))
 
 screen = pg.display.set_mode(reso)
 screen.fill(grey)
 
 running = True
-in_menu = True
-in_exit_menu = False
-in_instructions = False
+state = "main"
 
 while running:
     # Draw all the things
     draw_background()
-    if in_menu:
-        draw_menu()
-    elif in_exit_menu:
-        draw_exit_menu()
+    for text in texts:
+        if text.name.split('_')[0] == state:
+            text.draw()
     pg.display.flip()
 
     # Start of the commands
     pg.event.pump()
     keys = pg.key.get_pressed()
 
-    if in_menu:
+    if state == "main":
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
@@ -83,13 +71,11 @@ while running:
                 print("Started new game")
 
             if event.type == pg.KEYDOWN and event.key == pg.K_i:
-                print("Opened instructions")
+                state = "instructions"
 
             if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
-                in_exit_menu = True
-                in_menu = False
-
-    if in_exit_menu:
+                state = "exit"
+    elif state == "exit":
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
@@ -100,6 +86,14 @@ while running:
                 pg.quit()
 
             elif event.type == pg.KEYDOWN and event.key == pg.K_n:
-                in_menu = True
-                in_exit_menu = False
+                state = "main"
+    elif state == "instructions":
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+                pg.quit()
+
+            if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
+                state = "main"
+
 pg.quit()
